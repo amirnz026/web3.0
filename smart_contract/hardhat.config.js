90201156a4d95e3e772b1b0a2e1911ffff2bcfd6
Amir Hosein Nazari <czhablog@gmail.com>
2022-10-05 12:36:13 +0400

    fix: fixed credentials leak

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,15 +1,15 @@
 // https://eth-goerli.g.alchemy.com/v2/SI1LCxnPJafOPPe2GW_p4hPq9lhCOk_N
 require("@nomicfoundation/hardhat-toolbox");
 // require("@nomicfoundation/hardhat-chai-matchers");
+const GOERLI_URL = import.meta.env.GOERLI_URL;
+const METAMASK_ACCOUNT = import.meta.env.METAMASK_ACCOUNT;
 
 module.exports = {
   solidity: "0.8.0",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/SI1LCxnPJafOPPe2GW_p4hPq9lhCOk_N",
-      accounts: [
-        "158d204d92c94a561d2a11616dd4e760a5d0426090f9909aa1c19663676abb64",
-      ],
+      url: GOERLI_URL,
+      accounts: [METAMASK_ACCOUNT],
     },
   },
 };
